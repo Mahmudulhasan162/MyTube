6a364c2a9b693abe6b4bbb5d355f4da04aa9144e
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2023-09-01 01:08:54 +0600

    working

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const handleTab = async() =>{
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data= await response.json()
-    console.log(data.data);
+    // console.log(data.data);
     const categoriesArray= data.data;
 
 
@@ -10,14 +10,14 @@ const handleTab = async() =>{
     categoriesArray.forEach((category) => {
         const div= document.createElement('div');
         div.innerHTML=`
-        <a class="tab" onclick="handleCards(${category.category_id})">${category.category}</a>
+        <a class=" tab px-4  py-1 text-black bg-gray-300 hover:text-white hover:bg-redColor rounded-lg mx-4"onclick="handleCards(${category.category_id})">${category.category}</a>
         `;
     tabContainer.appendChild(div);
     });
 };
 
 const handleCards =async(cards) => {
-    console.log(cards);
+    // console.log(cards);
     const response= await fetch(`https://openapi.programming-hero.com/api/videos/category/${cards}`)
     const data = await response.json();
 
@@ -27,28 +27,51 @@ const handleCards =async(cards) => {
 
     cardContainer.innerHTML="";
 
-    console.log(cardsArray);
+    const noCard= document.getElementById('no-card')
+    noCard.innerHTML='';
 
-    cardsArray.forEach(element => {
-        console.log(element);
-        const div= document.createElement('div');
-    div.innerHTML=`
-    <div class="card bg-base-100 shadow-xl">
-          <figure><img src=${element.thumbnail} /></figure>
-          <div class="card-body">
-          
-          <div class="flex flex-row space-x-1">
-          <figure><img class="w-10 rounded-full" src="${element.authors[0].profile_picture}" alt=""></figure>
-        <h2 class="card-title">${element.title}</h2>
-      </div>
-            <p class="text-sm font-normal text-gray-400 ">${element.authors[0].profile_name
-            } <span class="ml-2">${element.authors[0].verified }</span></p>
-            <p class="text-sm font-normal text-gray-400">${element.others.views} views</p>
-          </div>
+    if(cardsArray.length==0){
+        
+        const section= document.createElement('div');
+        section.innerHTML=`
+        <div class="container mx-auto text-center w-[400px] space-y-6 ">
+        <img class="mx-auto" src="./images/Icon.png" alt="">
+          <h2 class="text-3xl font-bold">Oops!! Sorry, There is no content here</h2>
         </div>
-    `;
-    cardContainer.appendChild(div)
-    });
+        `;
+        noCard.appendChild(section)
+    }
+    else{
+        
+        cardsArray.forEach(element => {
+            // console.log(element);
+            const div= document.createElement('div');
+        div.innerHTML=`
+        <div class="card w-full h-[350px] ">
+              <figure><img class="w-full max-h-[150px]"  src=${element.thumbnail} /></figure>
+              <div class="card-body">
+              
+              <div class="flex flex-row space-x-4">
+              <figure><img class="w-10 h-10 rounded-full" src="${element.authors[0].profile_picture}" alt=""></figure>
+            <h2 class="card-title">${element.title}</h2>
+          </div>
+                <div class="flex flex-row gap-3">
+                <div><p class="text-sm font-normal text-gray-400 ">${element.authors[0].profile_name
+                } </p></div>
+                <div>${element.authors[0].verified?'<img src="./images/fi_10629607.svg" alt="">': '' }
+                </div>
+    
+                </div>
+                <p class="text-sm font-normal text-gray-400">${element.others.views} views</p>
+              </div>
+            </div>
+        `;
+        cardContainer.appendChild(div)
+        });
+    }
+    // console.log(cardsArray);
+
+    
 
     
 }
